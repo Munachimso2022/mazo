0826f66b03cfae9182459131d5e7289d194d99e6
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-13 16:30:09 +0100

    M

--- resources/views/pages/profile/_more.blade.php
+++ resources/views/pages/profile/_more.blade.php
@@ -16,16 +16,19 @@
     </div>
     <h3 class="card-title text-center">Your Wallet</h3>
       <span class="text-center"><b>{{$wallet->balace}}</b></span>
-  </div>
-  @if(count($funds)>0)
-  <div class="">
-    <ul class="list-group">
-    @foreach($funds as $fund)
-      <li class="list-group-item"><p class="text-center text-dark">Your ${{$fund->amount}} funding is pending admin verification. </p></li>
-    @endforeach
-    </ul>
-  </div>
-  @endif
-
+    
+    @if(count($funds)>0)
+      <div class="">
+        <ul class="list-group">
+        @foreach($funds as $fund)
+          <li class="list-group-item"><p class="text-center text-dark">Your ${{$fund->amount}} funding is pending admin verification. </p></li>
+        @endforeach
+        </ul>
+      </div>
+    @endif
+    <div class="card-footer">
+      <p>Refer with your referral code and earn: {{$wallet->user->my_code}}</p>
+    </div>
+</div>
 </div>
 
