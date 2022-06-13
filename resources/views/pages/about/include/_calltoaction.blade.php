382c29396223451652fb89dad6b8fb795c383e70
Uchenna Munachimso Nwachukwu <99395535+Munachimso2022@users.noreply.github.com>
2022-06-14 00:33:01 +0100

    Update _calltoaction.blade.php

--- resources/views/pages/about/include/_calltoaction.blade.php
+++ resources/views/pages/about/include/_calltoaction.blade.php
@@ -4,7 +4,7 @@
 				<div class="col-12 text-center">
 					<div class="calltoaction-two-wrap mb-4">
 						<h4 class="text-uppercase">Need More Service?</h4>
-						<p>We make it our business to know what's important to you. Kindly hit our 24/7 live chat button.<br></p>
+						<p>We make it our business to know what's important to you. Kindly hit our 24/7 live chat button. You can call or chat us on Whatsapp and Telegram.<br></p>
 					</div>
 					<a href="#" class=" wow fadeInDown btn-style btn-border btn-border-3">Contact Us <i class="icofont icofont-hand-right"></i></a>
 				</div>
